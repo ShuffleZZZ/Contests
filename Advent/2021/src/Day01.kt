ff37f987831d2e32e37816f2b52bebc2ca58413e
ShuffleZZZ <mihpihnaty@yandex.ru>
2021-12-01 10:13:50 +0300

    add day 1

---
+++ Advent/2021/src/Day01.kt
@@ -0,0 +1,15 @@
+fun main() {
+  fun part1(input: List<Int>) = input.zipWithNext().count { (a, b) -> a < b }
+
+  fun part2(input: List<Int>) = input.zipWithNext().zipWithNext { (a, b), (_, d) -> listOf(a, b, d) }
+    .zipWithNext().count { (a, b) -> a.sum() < b.sum() }
+
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInt("input_test")
+  check(part1(testInput) == 7)
+  check(part2(testInput) == 5)
+
+  val input = readInt("input")
+  println(part1(input))
+  println(part2(input))
+}
---
+++ Advent/2021/src/Utils.kt
@@ -0,0 +1,18 @@
+import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = File("src/input", "$name.txt").readLines()
+
+/**
+ * Reads Int values placed one at each line.
+ */
+fun readInt(name: String) = readInput(name).map { it.toInt() }
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
