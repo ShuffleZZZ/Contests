f8a811fbfefacf93aeae9571300ed20abc55d3df
ShuffleZZZ <mihpihnaty@yandex.ru>
2021-12-08 22:16:38 +0300

    fix day 8 part 2

--- Advent/2021/src/Day08.kt
+++ Advent/2021/src/Day08.kt
@@ -32,9 +32,6 @@ fun findBottomLeft(input: List<Output>) = (SIX(input) - FIVE(input)).first()
 fun findBottomRight(input: List<Output>, known: Output) = (SEVEN(input) - known).single()
 fun findTopLeft(input: List<Output>, known: Output) = (EIGHT(input) - known).single()
 
-
-fun output(input: List<String>) = input.map { it.substringAfter('|').trim().split(' ') }
-
 private fun String.toNumber(mapping: List<Char>): Int = when {
   this.length == 2 -> 1
   this.length == 3 -> 7
@@ -42,26 +39,27 @@ private fun String.toNumber(mapping: List<Char>): Int = when {
   this.length == 7 -> 8
   this.length == 6 && mapping[4] !in this -> 9
   this.length == 6 && mapping[1] !in this -> 6
-  mapping[1] !in this -> 5
-  mapping[2] !in this -> 2
-  else -> 3
+  this.length == 5 && mapping[1] !in this && mapping[4] !in this -> 5
+  this.length == 5 && mapping[2] !in this && mapping[5] !in this -> 2
+  this.length == 5 && mapping[4] !in this && mapping[5] !in this -> 3
+  else -> 0
 }
 
 fun main() {
-  fun part1(input: List<String>) = output(input).flatten().count { it.length in listOf(2, 3, 4, 7) }
+  fun part1(input: List<String>) =
+    input.map { it.substringAfter('|').trim().split(' ') }.flatten().count { it.length in listOf(2, 3, 4, 7) }
 
-  fun part2(input: List<String>) = input.map { line -> line.split(" | ").map { it.split(" ") } }
-    .map { (left, right) ->
-      left.map { it.toSet() }.let { strings ->
-        MutableList(7) { 'ö' }.let {
-          it[0] = findTop(strings)
-          it[1] = findTopRight(strings)
-          it[3] = findBottom(it[0], strings)
-          it[4] = findBottomLeft(strings)
-          it[6] = findMid(it[3], strings)
+  fun part2(input: List<String>) = input.map { line -> line.split(" | ").map { it.split(" ") } }.map { (left, right) ->
+    left.map { it.toSet() }.let { strings ->
+      MutableList(7) { 'ö' }.let {
+        it[0] = findTop(strings)
+        it[1] = findTopRight(strings)
+        it[3] = findBottom(it[0], strings)
+        it[4] = findBottomLeft(strings)
+        it[6] = findMid(it[3], strings)
 
-          it[2] = findBottomRight(strings, it.filterNot('ö'::equals).toSet())
-          it[5] = findTopLeft(strings, it.filterNot('ö'::equals).toSet())
+        it[2] = findBottomRight(strings, it.filterNot('ö'::equals).toSet())
+        it[5] = findTopLeft(strings, it.filterNot('ö'::equals).toSet())
 
           it
         }.let { right.map { string -> string.toNumber(it) }.joinToString("").toInt() }
