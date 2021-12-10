750a8859c9a33699967b084c942986dee8e92cab
Shuffle@work <mihpihnaty@yandex.ru>
2021-12-10 17:57:55 +0300

    fix day 10 lint

--- Advent/2021/src/Day10.kt
+++ Advent/2021/src/Day10.kt
@@ -37,18 +37,17 @@ fun Char.matchingPoints() = when (this) {
 
 private fun ArrayDeque<Char>.safePop(it: Char) = if (this.last().matches(it)) this.removeLastOrNull() != null else false
 
-private fun ArrayDeque<Char>.complete(): List<Char> {
-  while (this.last().isOpen()) this.addFirst(this.removeLast().matching())
+private fun ArrayDeque<Char>.sumCompletionPoints() =
+  this.map { it.matching().matchingPoints().toLong() }.reversed().reduce { acc, br -> acc * 5 + br }
 
-  return this.toList().reversed()
-}
-
+fun parseEntry(stack: ArrayDeque<Char>, ind: Int, bracket: Char) =
+  if (bracket.isOpen()) stack.add(bracket).let { null } else if (stack.safePop(bracket)) null else ind
 
 fun main() {
   fun part1(input: List<String>) = input.map { string ->
     ArrayDeque<Char>().let { stack ->
       string.mapIndexedNotNull { ind, it ->
-        if (it.isOpen()) stack.add(it).let { null } else if (stack.safePop(it)) null else ind
+        parseEntry(stack, ind, it)
       }.firstOrNull()?.let { string[it] }
     }
   }.sumOf { it?.points() ?: 0 }
@@ -56,11 +55,10 @@ fun main() {
   fun part2(input: List<String>) = input.mapNotNull { string ->
     ArrayDeque<Char>().let { stack ->
       string.mapIndexedNotNull { ind, it ->
-        if (it.isOpen()) stack.add(it).let { null } else if (stack.safePop(it)) null else ind
+        parseEntry(stack, ind, it)
       }.let { if (it.isEmpty()) stack else null }
     }
-  }.map { it.complete().map { br -> br.matchingPoints().toLong() }.reduce { acc, br -> acc * 5 + br } }.sorted()
-    .let { it[it.size / 2] }
+  }.map(ArrayDeque<Char>::sumCompletionPoints).sorted().let { it[it.size / 2] }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("input_test")
